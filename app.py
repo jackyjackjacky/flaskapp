90f4586701fc54461575a38484652fa62d32ab92
jackyjackjacky <jcorn025@fiu.edu>
2021-01-30 22:38:06 -0500

    Modifying css and html

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify, redirect, render_template, session, url_for, Response, request
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello, World!'
+	return render_template('home.html', user_ip= flask.request.remote_addr, hostname= request.headers.get('Host'))
 
 if __name__ == "__main__":
     app.run()
