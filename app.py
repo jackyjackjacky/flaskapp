09ea162980def68db24320be1b10cf9e71ff5f62
jackyjackjacky <jcorn025@fiu.edu>
2021-01-30 23:41:00 -0500

    added basic information

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify, redirect, render_template, session, url_for, Response, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return render_template('home.html', user_ip= flask.request.remote_addr, hostname= request.headers.get('Host'))
+	return render_template('home.html', hostname= request.headers.get('Host'), user_IP= request.remote_addr)
 
 if __name__ == "__main__":
     app.run()
